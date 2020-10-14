da776a40ed6395be02fb914899cd54b35459b9ed
Lukkai <luki99942@gmail.com>
2020-10-14 21:33:43 +0200

    cw3 init

---
+++ Lab1/cw3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#3) (3p.) Ćwiczenia z programem find: napisz skrypt znajdujący programem find wszystkie pliki o zadanym
+#   rozszerzeniu lub masce, które były modyfikowane w ciągu ostatnich N dni i tworzący z nich archiwum tar 
+#   o zadanej nazwie. Mamy tu trzy parametry: maskę nazwy pliku, liczbę N i nazwę archiwum.
+#   Parametry są zadawane przez argumenty, to znaczy skrypt zawsze będzie wywołany z trzema parametrami. 
+
