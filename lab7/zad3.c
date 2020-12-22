6f71019f62b1211e1ed4d1d25fd8d213bca8c80a
Lukkai <lukaszchhh@gmail.com>
2020-12-22 21:45:13 +0100

    zad3 wesolych swiat

--- lab7/zad3.c
+++ lab7/zad3.c
@@ -15,21 +15,24 @@
 void *PrintHello(void *threadid)
 {
    long taskid;
+
    sleep(1);
-   taskid = *(long *)threadid;
+   taskid = (long) threadid;
    printf("Hello from thread %ld\n", taskid);
    pthread_exit(NULL);
 }
-  
+
 int main(int argc, char *argv[])
 {
 pthread_t threads[NUM_THREADS];
+long taskids[NUM_THREADS];
 int rc;
 long t;
 
 for(t=0;t<NUM_THREADS;t++) {
+  taskids[t] = t;
   printf("Creating thread %ld\n", t);
-  rc = pthread_create(&threads[t], NULL, PrintHello, (void *) &t);
+  rc = pthread_create(&threads[t], NULL, PrintHello, (void *) taskids[t]);
   if (rc) {
     printf("ERROR; return code from pthread_create() is %d\n", rc);
     exit(-1);
