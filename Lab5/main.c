5a39a68850ba2966698ab3a3fe60e41a3bf23a89
Lukkai <lukaszchhh@gmail.com>
2021-01-20 12:03:45 +0100

    w dzien sądu ostatecznego

--- Lab5/main.c
+++ Lab5/main.c
@@ -10,9 +10,40 @@
 #include <string.h>
 #include <unistd.h>
 
-#define MAX_BUFFFER 64
+#define BUFFER_SIZE 32
 
 int main(int argc, char *argv[])
 {
+    int pid;
+    int file_descriptor[2];
+    pipe(file_descriptor);
+
+    if (pid==fork())
+    {
+        char buf[BUFFER_SIZE];
+        while (read(file_descriptor[0], buf, BUFFER_SIZE))
+        {
+            if(buf[BUFFER_SIZE -1] == 0)
+                break;
+            buf[BUFFER_SIZE - 1] = '\0';
+            printf("#%s#\n", buf);
+        }
+    }
+    else
+    {
+        FILE *f = fopen(argv[1], "r");
+        char buf[BUFFER_SIZE];
+
+        while (fgets(buf, BUFFER_SIZE - 1, f))
+        {
+            buf[BUFFER_SIZE - 1] = 1;
+            write(file_descriptor[1], buf, BUFFER_SIZE);
+        }
+
+        buf[BUFFER_SIZE - 1] = 0;
+        write(file_descriptor[1], buf, BUFFER_SIZE);
+
+        fclose(f);
+    }
 
 }
